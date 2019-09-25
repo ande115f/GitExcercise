8ca452f99e61b0b7ed50d33bdd86a0eb83088526
Nico <55789582+NaishTV@users.noreply.github.com>
2019-09-25 14:00:12 +0200

    blah

--- src/P3_33.java
+++ src/P3_33.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class P3_33 {
     public static void main(String[] args) {
 
+        //Declaration
+
+        int satisfaction = 0;
+        double costOfMeal = 0;
+        double tip = 0;
+
+        System.out.println("Price Total:___________________________________ " + costOfMeal);
+        System.out.println("_________________________________________________________");
+        System.out.println("1 = Totally satisfied | 2 = Satisfied | 3 = Dissatisfied");
+        System.out.println("Please enter your Satisfaction Level: ");
+
+        Scanner sc = new Scanner(System.in);
+        satisfaction = sc.nextInt();
+
+        if (satisfaction == 1) {
+            ///Kappa12
+        }
     }
 }
