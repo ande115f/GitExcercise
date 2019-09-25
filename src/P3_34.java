9b34b2281413fb09e2a5f7a8801244f0ff202885
BLVCKMVK <4511154-BLVCKMVK@users.noreply.gitlab.com>
2019-09-25 14:18:45 +0200

    Blah blah

--- src/P3_34.java
+++ src/P3_34.java
@@ -5,21 +5,44 @@ public class P3_34
     public static void main(String[] args)
     {
 
-        double totalDiscount = -1;
+        // Declarations
+        double totalDiscount = 0;
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Discount calculator!");
         System.out.println("Please enter the total cost of your groceries:");
         double groceriesPrice = keyboard.nextDouble();
 
-        if (groceriesPrice > 10) {
+        // Primary calculations
+        if (groceriesPrice < 10)
+        {
             totalDiscount = 0;
-        } else if (groceriesPrice < 10 && groceriesPrice > 60){
+        }
+        else if (groceriesPrice < 10 || groceriesPrice > 60)
+        {
             totalDiscount = 0.08;
-        } else if ()
+        }
+        else if (groceriesPrice < 60 || groceriesPrice >= 150)
+        {
+            totalDiscount = 0.10;
+        }
+        else if (groceriesPrice <= 150 || groceriesPrice >= 210)
+        {
+            totalDiscount = 0.12;
+        }
+        else if (groceriesPrice > 210)
+        {
+            totalDiscount = 0.14;
+        }
 
-
-        System.out.println("You're entitled to a discount coupon of " + totalDiscount + "%");
-
+        // Output
+        if (totalDiscount == 0)
+        {
+            System.out.println("You're not entitled to a discount.");
+        }
+        else
+            {
+                System.out.println("You're entitled to a discount coupon of " + (totalDiscount*groceriesPrice) + "$ (That's " + (totalDiscount*100) + "% of your price!");
+            }
     }
 }
