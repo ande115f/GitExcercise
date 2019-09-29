41708d3ea2416eedf6738821871be168c3c06cbb
linerebo <55693839+linerebo@users.noreply.github.com>
2019-09-29 17:53:20 +0200

    Test

--- src/P3_31.java
+++ src/P3_31.java
@@ -1,7 +1,9 @@
+//Exercise P3.31 p131.
 import java.util.Scanner;
 
 public class P3_31 {
     public static void main(String[] args) {
+
         //ask user for input
         Scanner in = new Scanner(System.in);
         System.out.print("Please input your Name: ");
@@ -14,12 +16,18 @@ public class P3_31 {
         //calculation of pay
         double payOvertimerWork = 0;
         double pay = hourlyWage * workHours;
+
         if (workHours > 40) {
             double payOvertimeWork = (workHours - 40) * (1.5 * hourlyWage);
         }
         double payTotal = pay + payOvertimerWork;
-        System.out.print(payTotal);
 
-
+        //output paycheck
+        System.out.println("------------------------------------");
+        System.out.println("Paycheck for: "+name);
+        System.out.println("Hourly wage: "+hourlyWage+ " Euro");
+        System.out.println("Workhours for the week: "+workHours+" hours");
+        System.out.print("Paycheck for the week in Euro: ");
+        System.out.printf("%.2f",payTotal);
     }
 }
